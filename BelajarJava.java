a4a1067547a07c62c593162c3022fcf0888bb659
bvvvaaaa <141114677+bvvvaaaa@users.noreply.github.com>
2023-08-01 13:23:35 +0700

    asd
    sasda

---
+++ BelajarJava.java
@@ -0,0 +1,9 @@
+import BelajarJava.Komputer;
+
+ class BelajarJava{
+  public static void main(String args[]){
+
+    Komputer KomputerSaya = new Komputer();
+     System.out.println(KomputerSaya.cekInfo());
+   }
+}
